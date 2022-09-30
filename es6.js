0b227140f9eac9a72fabebd1281128fbad91ffdf
Akramsarker <dev.mdakram@gmail.com>
2022-10-01 00:44:33 +0600

    Solved this problem write a arrow functions with parameters

--- es6.js
+++ es6.js
@@ -42,3 +42,10 @@ const magic = () => {
   return new Date();
 };
 console.log(magic());
+
+// Write Arrow Functions with Parameters
+const myConcat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
