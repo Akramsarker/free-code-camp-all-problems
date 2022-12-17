3cda81a4e65c298677876245e9d8542aa6b3088a
Akramsarker <dev.mdakram@gmail.com>
2022-12-18 00:15:06 +0600

    Solve problem mutate an array declared with const

--- es6.js
+++ es6.js
@@ -10,3 +10,13 @@ function checkScope() {
 }
 
 checkScope();
+
+// Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+}
+console.log(editInPlace());
+console.log(s);
