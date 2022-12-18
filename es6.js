e6ec4c54fc1edcae6cc302dea2179a73f79be63f
Akramsarker <dev.mdakram@gmail.com>
2022-12-19 00:26:29 +0600

    Solve this challenge prevent object mutation

--- es6.js
+++ es6.js
@@ -20,3 +20,19 @@ function editInPlace() {
 }
 console.log(editInPlace());
 console.log(s);
+
+// Prevent Object Mutation
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  Object.freeze(MATH_CONSTANTS);
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+console.log(PI);
