594304a1640e5668e3987ab3efa601ca51a28421
Akramsarker <dev.mdakram@gmail.com>
2023-04-24 03:44:02 +0600

    Compound assignment aith augmented division

--- basic-javaScript.js
+++ basic-javaScript.js
@@ -119,6 +119,15 @@ b *= 3;
 c *= 10;
 console.log(a, b, c);
 
+// Compound Assignment With Augmented Division(24)
+let a = 48;
+let b = 108;
+let c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+console.log(a, b, c);
+
 // Problem Number of 63
 function testNotEqual(val) {
   if (val != 99) {
