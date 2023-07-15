67ed4bc5a25a1ed88e2ab3f67b7854998de28d45
Akramsarker <dev.mdakram@gmail.com>
2023-07-15 17:46:05 +0600

    Create strings using template literals

--- es6.js
+++ es6.js
@@ -131,3 +131,20 @@ const stats = {
   average: 35.85,
 };
 const half = ({ max, min }) => (max + min) / 2.0;
+
+// Create Strings using Template Literals(14)
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"],
+};
+function makeList(arr) {
+  "use strict";
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
